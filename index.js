d62789e3cf59b966616e46a9b679b184d1c01c49
NabihaZeeshan <zeeshannabiha88@gmail.com>
2024-09-19 20:11:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+//QUESTION NO 1//
+let myname="nabiha";
+alert (myname);
+
+//QUESTION NO 2//
+var fullname ="nabiha zeeshan";
+alert (fullname);
+console.log("my name is" +" "+ fullname);
+
+//QUESTION NO 3//
+let a=4;
+let b=4;
+let total = (a++ + --b - --a + b++ + a + b);
+console.log(total);
+
+//QUESTION NO 4//
+let age = prompt ("how old are you");
+if (age=5){
+    alert("you are allowed in school")
+}
+else if (age<=5){
+    alert("you are allowed in KG-1");
+}
+else if(age>=5){
+    alert("you are allowed in nursery");
+}
+else {
+    alert ("you are not allowed");
+}
+
+
+
+
+
